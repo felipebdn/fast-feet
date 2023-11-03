454801cf3cad7d5a1639cdb549ca95b49884d7ce
Felipe Barbosa do Nascimento <dfelipebdn@gmail.com>
2023-11-03 19:43:09 -0300

    teste da implementação de domain-events

--- ddd/src/core/events/domain-event.spec.ts
+++ ddd/src/core/events/domain-event.spec.ts
@@ -1,6 +1,8 @@
 import { Entity } from '../entities/entity'
 import { UniqueEntityID } from '../entities/unique-entity-id'
 import { DomainEvent } from './domain-event'
+import { DomainEvents } from './domain-events'
+import { vi } from 'vitest'
 
 class CustomEntityCreated implements DomainEvent {
   public ocurredAt: Date
@@ -21,9 +23,29 @@ class CustomEntity extends Entity<null> {
     const entity = new CustomEntity(null)
 
     entity.addDomainEvent(new CustomEntityCreated(entity))
+
+    return entity
   }
 }
 
 describe('Domain Events', () => {
-  it('should be able to dispatch and listem to events', async () => {})
+  it('should be able to dispatch and listem to events', async () => {
+    const callbackSpy = vi.fn()
+
+    // Subscribe cadastrado (ouvindo o evento de "resposta criada")
+    DomainEvents.register(callbackSpy, CustomEntityCreated.name)
+
+    // Estou criando uma reposta porém SEM salvar no canco
+    const entity = CustomEntity.create()
+
+    // Estou assegurando que o evento foi criado porém NÃO foi disparado
+    expect(entity.domainEvents).toHaveLength(1)
+
+    // Estou salvando a resposta no banco de dados e assim disparando evento
+    DomainEvents.dispatchEventsForEntity(entity.id)
+
+    // O subscriber ouve o evento e faz o que precisa ser feito com o dado
+    expect(callbackSpy).toHaveBeenCalled()
+    expect(entity.domainEvents).toHaveLength(0)
+  })
 })
