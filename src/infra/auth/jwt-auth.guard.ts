298dd782e2e1c9b11cf3a2e527e4933c6d4e0188
Felipe Barbosa <dfelipebdn@gmail.com>
2023-11-24 18:02:14 -0300

    180224112023

--- src/infra/auth/jwt-auth.guard.ts
+++ src/infra/auth/jwt-auth.guard.ts
@@ -1,3 +1,34 @@
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
 import { AuthGuard } from '@nestjs/passport'
+import { Observable } from 'rxjs'
 
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super()
+  }
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler())
+
+    if (!roles) {
+      return true // Se não houver restrições de função, permita o acesso (membros padrão)
+    }
+
+    const request = context.switchToHttp().getRequest()
+    const user = request.user
+
+    if (!user || !user.role) {
+      throw new UnauthorizedException('Unauthorized access.')
+    }
+
+    return roles.includes(user.role)
+  }
+}
